5c90e94169abe4c4eea6be3a5cb2b62e527682a9
Youssef Mostafa <youssefm32@nycstudents.net>
2025-09-11 07:05:39 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from turtle import *
 t = Turtle()
 
 t.shape('turtle')
-t.speed(40)
+t.speed(100)
 """ def square(x):
     for i in range(60):
         t.forward(x)
@@ -36,14 +36,57 @@ t.left(90) """
     t.forward(x)
 equal(90)  """
 
-def square(x):
+
+
+
+
+def star(x, y):
+    for i in range(5):
+        t.forward(x)
+        t.left(y)
+
+""" def doubleSquares(iRange):
+    sidelength = 25
+    for i in range(iRange):
+        square(sidelength)
+        sidelength = sidelength * 2
+doubleSquares(5) """
+
+
+def addStars(iRange):
+    length = 5
+    for i in range(iRange):
+        star(length, 144)
+        length += 5
+        t.right(5)
+addStars(60)
+
+
+""" def message(input):
+    print(input)
+message("Hello Class") """
+
+
+""" def square(x, y):
     def addSquares(iRange):
         length = 25
     for i in range(iRange):
         square(length, 90)
         length += 25
-addSquares(5)
+        square(50)
+    addSquares(5) """
 
-square(50)
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+print(i) """
 
 turtle.done()
