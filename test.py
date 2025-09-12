77a0dd32235c040593fcaba80153c10e780b4596
Youssef Mostafa <youssefm32@nycstudents.net>
2025-09-12 10:32:03 -0400

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ t.speed(100)
         t.right(5)
 square(200) """
 
-
+#rectangle code for challenge
 
 """ t.forward(100)
 t.left(90)
@@ -27,6 +27,8 @@ t.forward(100)
 t.left(90)
 t.forward(125)
 t.left(90) """
+
+# triangle code for challenge
 
 """ def equal(x):
     t.forward(x)
@@ -39,11 +41,22 @@ equal(90)  """
 
 
 
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(90)  """
 
-def star(x, y):
+
+
+#star code for challenge
+
+""" def star(x, y):
     for i in range(5):
         t.forward(x)
-        t.left(y)
+        t.left(y) """
 
 """ def doubleSquares(iRange):
     sidelength = 25
@@ -53,13 +66,13 @@ def star(x, y):
 doubleSquares(5) """
 
 
-def addStars(iRange):
+""" def addStars(iRange):
     length = 5
     for i in range(iRange):
         star(length, 144)
         length += 5
         t.right(5)
-addStars(60)
+addStars(60) """
 
 
 """ def message(input):
@@ -67,26 +80,16 @@ addStars(60)
 message("Hello Class") """
 
 
-""" def square(x, y):
-    def addSquares(iRange):
-        length = 25
-    for i in range(iRange):
-        square(length, 90)
-        length += 25
-        square(50)
-    addSquares(5) """
+def square(x):
+    for i in range(4)
+    t.forward(x)
+    t.left(90)
+def
 
-""" def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
 
-print(i) """
+
+
+
+
 
 turtle.done()
